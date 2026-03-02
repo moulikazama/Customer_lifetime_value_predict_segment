e718f8114bc1265d896291333e8ea741e2f1bf78
moulikazama <moulikazama@gmail.com>
2026-03-02 16:09:39 +0530

    Notebook 4 and clustering.py updated

--- src/.ipynb_checkpoints/clustering-checkpoint.py
+++ src/.ipynb_checkpoints/clustering-checkpoint.py
@@ -1,12 +1,20 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
-def scale_features(rfm):
+def scale_features(data):
     scaler = StandardScaler()
-    scaled = scaler.fit_transform(rfm[['Recency','Frequency','Monetary']])
+    num_col = data.select_dtypes(include=['int64','float64'])
+    scaled = scaler.fit_transform(num_col)
     return scaled
 
-def apply_kmeans(data, k=4):
+def apply_kmeans(data, k):
     model = KMeans(n_clusters=k, random_state=42)
     labels = model.fit_predict(data)
     return model, labels
+
+def sil_score(data, k):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    labels = kmeans.fit_predict(data)
+    score = silhouette_score(data, labels)
+    print(f"K={k}, Silhouette Score={score}")
