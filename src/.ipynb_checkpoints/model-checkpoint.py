64286b300decd1d4c3cee8663e46cdaa62f76031
moulikazama <moulikazama@gmail.com>
2026-03-02 23:22:07 +0530

    Preprocess: optimized, debugged. CLV evaluated. CLV_predicted_model and powerBI dashboard pending

--- src/.ipynb_checkpoints/model-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def calculate_clv(rfm):
-
-    rfm['AOV'] = rfm['Monetary'] / rfm['Frequency']
-    rfm['Lifespan_Score'] = 365 - rfm['Recency']
-
-    rfm['CLV'] = rfm['AOV'] * rfm['Frequency'] * (rfm['Lifespan_Score'] / 365)
-
-    return rfm
